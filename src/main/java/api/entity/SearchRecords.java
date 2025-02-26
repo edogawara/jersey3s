7e79c6d7108d2ea1cd2cb7c3ea7ff6df9daa38b6
baba <baba@N100DC>
2025-02-26 17:26:48 +0900

    修正

---
+++ src/main/java/api/entity/SearchRecords.java
@@ -0,0 +1,41 @@
+package api.entity;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class SearchRecords {
+	
+	public ArrayList<LinkedHashMap<String,String>> records = new ArrayList<>();
+	
+	public SearchRecords(Connection conn, String tableName, HashMap<String,String> cond ) throws SQLException  {
+		
+		String query = cond.get("query");
+		String where = "";
+		if( query != null && !query.trim().equals("")) {
+			where = " where (" + query + ") ";
+		}
+		String sql = "select * from " + tableName + where ;
+		
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		ResultSetMetaData meta = rs.getMetaData();
+			
+		while( rs.next() ) {
+				
+			LinkedHashMap<String, String> map = new LinkedHashMap<>();
+			for( int i = 0; i < meta.getColumnCount(); i++ ) {
+				String name = meta.getColumnName(i+1);
+				String val = rs.getString(name);
+				map.put(name, val);
+			}
+			records.add(map);
+		}
+		rs.close();
+	}
+}
